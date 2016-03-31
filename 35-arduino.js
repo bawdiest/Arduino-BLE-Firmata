fe2c65b458c2f602418cf705f187db6023d36a3e
bawdiest <mikmak@Mikhails-MacBook-Pro.local>
2016-03-31 21:39:01 +0200

    Roll Back to 0.0.7

--- 35-arduino.js
+++ 35-arduino.js
@@ -121,13 +121,6 @@ module.exports = function(RED) {
 			});
 
 			node.on("input", function(msg) {
-				var BleUart = require('/home/pi/BluetoothLE-Examples/noble/readSerial/ble-uart.js');
-						var uart = {
-			serviceUUID : '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
-			txUUID : '6e400002-b5a3-f393-e0a9-e50e24dcca9e',
-			rxUUID : '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
-		}
-		node.board = new BleUart('UART', uart);
 
 				node.board.write(msg.payload);
 
